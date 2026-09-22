f8cbd192037d47677362a7cd51ee3636fd2ff1e8
biomedmoon <biomedmoonpa@gmail.com>
2026-09-22 15:55:29 -0400

    Update Actual_Test.js
    global listener added

--- Actual_Test.js
+++ Actual_Test.js
@@ -1510,16 +1510,32 @@
         document.body.appendChild(ov);
     }
 
-    window.addEventListener('keydown', (e) => {
-        if (e.altKey && e.code === customHotkey) {
-            e.preventDefault();
-            let existingModal = document.getElementById('preset-notes-modal-test');
-            if (existingModal) existingModal.remove();
-            else showMainModal();
-        }
-    });
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', () => {
+            window.addEventListener('keydown', (e) => {
+                if (e.altKey && e.code === customHotkey) {
+                    e.preventDefault();
+                    let existingModal = document.getElementById('preset-notes-modal-test');
+                    if (existingModal) existingModal.remove();
+                    else showMainModal();
+                }
+            });
 
-    checkWhatsNew();
-    showMainModal();
+            checkWhatsNew();
+            showMainModal();
+        });
+    } else {
+        window.addEventListener('keydown', (e) => {
+            if (e.altKey && e.code === customHotkey) {
+                e.preventDefault();
+                let existingModal = document.getElementById('preset-notes-modal-test');
+                if (existingModal) existingModal.remove();
+                else showMainModal();
+            }
+        });
+
+        checkWhatsNew();
+        showMainModal();
+    }
 
 })();
